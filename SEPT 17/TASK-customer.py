5253a4e0126380c35b85d7d906160c2a60a9d28a
Vignesh S <128490377+vignesh613@users.noreply.github.com>
2025-09-17 15:12:20 +0530

    Create TASK-customer.py

---
+++ SEPT 17/TASK-customer.py
@@ -0,0 +1,13 @@
+class Customer:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def name(self):
+        return f"my Firstname is {self.firstname} and my Lastname is {self.lastname}"
+      
+c1 = Customer('Arun','Kumar')
+c2 = Customer('Jasmine','Priya')
+
+print(c1.name())
+print(c2.name())
